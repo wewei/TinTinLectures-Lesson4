99bc3dd4d8953a79efb494678b97bebea3316ce8
Wei Wei <weiwei@geekteahouse.cn>
2022-03-05 13:09:01 +0800

    Initial commit.

---
+++ src/Lesson4_assets/src/index.js
@@ -0,0 +1,19 @@
+import { Lesson4 } from "../../declarations/Lesson4";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Lesson4.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
